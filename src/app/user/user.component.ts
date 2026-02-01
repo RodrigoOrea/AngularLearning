566a166acef7a6f24636145a51fa045d5ecfb75f
RodrigoOrea <rodrigosiete@outlook.es>
2026-02-01 17:23:43 +0100

    01022026 work

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,19 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, input, computed, Output, EventEmitter, output} from '@angular/core';
 import { DUMMY_USERS } from '../users/dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+// type User = {
+//   id:string;
+//   avatar:string
+//   name:string
+// }
+
+interface User {
+  id:string;
+  avatar:string
+  name:string
+}
 
 @Component({
   selector: 'app-user',
@@ -12,12 +24,28 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 })
 export class UserComponent {
 
-  @Input() avatar!: string
-  @Input() name!: string
+  // @Input({required: true}) id!: string
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
+  // @Output() select = new EventEmitter()
+
+  @Input({required: true}) user!: User
+
+  select = output<string>()
+  selectName = output<string>()
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
   onSelectUser() {
+    this.selectName.emit(this.user.id)
   }
 
-  get imagePath(){
-    return 'assets/users/' + this.avatar
-  }
+  get imagePath() {
+     return 'assets/users/' + this.user.avatar
+   }
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar()
+  // })
 }
